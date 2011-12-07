79db52a2a574363c0a3d1edc9cd6edf15993085b
Michelle Zhang <snowzhang2005@gmail.com>
2011-12-07 17:36:08 -0500

    change the FillInForm

--- src/java/FillInForm.java
+++ src/java/FillInForm.java
@@ -36,158 +36,34 @@ public class FillInForm extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
-        java.util.Date utilDate = new java.util.Date();
-        java.sql.Date sqlDate = new java.sql.Date(utilDate.getTime());
-        int id = -1;
-        String volunteerIdStr = null;
-        String volunteerName = null;
-        //Date startTime;
-        //Date endTime;
-        String streetName = null;
-        String houseNum = null;
-        String apt = null;
-        String city = null;
-        String state = null;
-        String zip = null;
-        String address = null;
-        String firstName = null;
-        String lastName = null;
-        String dwellingType = null;
-        String ownerInfo = null;
-        String landlordName = null;
-        String landlordPhone = null;
-        String insuranceInfo_f = null;
-        String insuranceInfo_s = null;
-        String insuranceInfo_c = null;
-        String floorNum = null;
-        String isBasement = null;
-        String isBasementOccupied = null;
-        String basementComment = null;
-        String waterLiving = null;
-        String waterBasement = null;
-        String isElectricOn = null;
-        String isGasOn = null;
-        String Electrical_service_box = null;
-        String Furnace = null;
-        String Heat_Water_Heater = null;
-        String Washer = null;
-        String Dryer = null;
-        String Stove = null;
-        String Regfrigerator = null;
-        String classification = null;
-        String reason = null;
-        String comments = null;
-        int num_of_floor = -1;
-        int waterLivingInt = -1;
-        int waterBasementInt = -1;
-
-
         String jsonArray = (String) request.getParameter("recordJSON");
-
-        String idsInserted = "";
-
         try {
+            Class.forName("com.mysql.jdbc.Driver");
+            String connectionStr = "jdbc:mysql://localhost/DisasterAssessment";
+            con = DriverManager.getConnection(connectionStr, dbuser, dbpw);
             JSONArray x = new JSONArray(jsonArray);
             for (int i = 0; i < x.length(); i++) {
                 JSONObject j = x.getJSONObject(0);
 
-                id = (Integer) j.get("id");
-                System.out.println("id = " + id);
-                volunteerIdStr = (String) j.get("volunteer_id");
-                int volunteerId = Integer.parseInt(volunteerIdStr);
-                volunteerName = (String) j.get("volunteer_name");
-                //Date startTime= (Date)j.get("start_time");
-                //Date endTime= (Date)j.get("end_time");
-                streetName = (String) j.get("street_name");
-                houseNum = (String) j.get("tel_house_num");
-                address = houseNum+" "+streetName;
-                apt = (String) j.get("tel_apt");
-                city = (String) j.get("txt_city");
-                state = (String) j.get("txt_state");
-                zip = (String) j.get("tel_zip");
-                firstName = (String) j.get("txt_first_name");
-                lastName = (String) j.get("txt_last_name");
-                landlordName = (String) j.get("landlord_name");
-                landlordPhone = (String) j.get("landlord_tel");
-                dwellingType = (String) j.get("dwelling_type");
-                insuranceInfo_f = (String) j.get("insurance_information_f");
-                insuranceInfo_s = (String) j.get("insurance_information_s");
-                insuranceInfo_c = (String) j.get("insurance_information_c");
-                ownerInfo = (String) j.get("owner_information");
-                floorNum = (String) j.get("number_floors");
-                if (floorNum != null && !floorNum.equals("")) {
-                    num_of_floor = Integer.parseInt(floorNum);
+                int flag = StoreData(j, con);
+                if (flag == 1) {
+                    request.setAttribute("FormSubmitMessage", "Your Assessment Form has been submitted successfully!");
+                } else {
+                    request.setAttribute("FormSubmitMessage", "You failed to submit Assessment Form.");
                 }
-                isBasement = (String) j.get("is_basement");
-                isBasementOccupied = (String) j.get("is_basement_occupied");
-                basementComment = (String) j.get("txtarea_basement_comment");
-                waterLiving = (String) j.get("water_in_living_area");
-                if (waterLiving != null && !waterLiving.equals("")) {
-                    waterLivingInt = Integer.parseInt(waterLiving);
-                }
-
-                waterBasement = (String) j.get("water_in_basement");
-                if (waterBasement != null && !waterBasement.equals("")) {
-                    waterBasementInt = Integer.parseInt(waterBasement);
-                }
-                isElectricOn = (String) j.get("electric");
-                isGasOn = (String) j.get("gas");
-                Electrical_service_box = (String) j.get("d_electrical");
-                Furnace = (String) j.get("d_furnace");
-                Heat_Water_Heater = (String) j.get("d_water");
-                Washer = (String) j.get("d_washer");
-                Dryer = (String) j.get("d_dryer");
-                Stove = (String) j.get("d_stove");
-                Regfrigerator = (String) j.get("d_fridge");
-                classification = (String) j.get("select_classification");
-                reason = (String) j.get("txtArea_classification_reason");
-                comments = (String) j.get("txtArea_comment");
-
-//            }
-//        } catch (JSONException e) {
-//            request.setAttribute("FormSubmitMessage", "You failed to submit the form!");
-//            //System.out.println(e);
-//        }
-//
-//        try {
-                Class.forName("com.mysql.jdbc.Driver");
-                String connectionStr = "jdbc:mysql://localhost/DisasterAssessment";
-                con = DriverManager.getConnection(connectionStr, dbuser, dbpw);
-                Client client = new Client(address, apt, city, state, zip, "", "", lastName, firstName);
-                client.Insert(con);
-                int clientId = client.getId(con);
-                System.out.println("clientId:" + clientId);
-
-                Building building = new Building(landlordName, landlordPhone, dwellingType, insuranceInfo_f, insuranceInfo_s, insuranceInfo_c, ownerInfo);
-                building.Insert(con);
-
-                int buildId = building.getId(con);
-                System.out.println("BuildingId:" + buildId);
-
-                DamageAssessment ds = new DamageAssessment(classification, "", "", num_of_floor, isBasement, waterLivingInt, waterBasementInt, isGasOn, isElectricOn, isBasementOccupied, basementComment, reason,
-                        Electrical_service_box, Furnace, Heat_Water_Heater, Washer, Dryer, Stove, Regfrigerator, sqlDate, sqlDate);
-                ds.insert(con);
-                int damageAssessmentId = ds.getId(con);
-
-                Cases caseInstance = new Cases(comments, clientId, damageAssessmentId, buildId, volunteerId);
-                caseInstance.Insert(con);
-
-                idsInserted = idsInserted + id + ",";
+                String destination = "/welcome.jsp";
+                //request.setAttribute("FormSubmitMessage", "Your Assessment Form has been submitted successfully!");
+                RequestDispatcher red = getServletContext().getRequestDispatcher(destination);
+                red.forward(request, response);
             }
-            request.setAttribute("submitStatus", "OK");
-            request.setAttribute("idsInserted", idsInserted);
-            request.setAttribute("formSuccessMessage", "The disaster assessment record has been sent to the server successfully.");
-
         } catch (Exception e) {
-            System.out.println(e.getMessage());
-            request.setAttribute("submitStatus", "ERROR");
-            request.setAttribute("idsInserted", idsInserted);
-            request.setAttribute("formFailureMessage", "The disaster assessment could not be saved to the server at this time. The assessment was stored locally on your device. Please visit the Sync page to try again!");
-        } finally {
+            request.setAttribute("FormSubmitMessage", "You failed to submit the form!");
             String destination = "/welcome.jsp";
             //request.setAttribute("FormSubmitMessage", "Your Assessment Form has been submitted successfully!");
             RequestDispatcher red = getServletContext().getRequestDispatcher(destination);
             red.forward(request, response);
+            System.out.println(e);
+        } finally {
             try {
                 if (con != null) {
                     con.close();
@@ -196,10 +72,88 @@ public class FillInForm extends HttpServlet {
                 Logger.getLogger(FillInForm.class.getName()).log(Level.SEVERE, null, ex);
             }
         }
+    }
 
+    public int StoreData(JSONObject j, Connection con) {
+        java.util.Date utilDate = new java.util.Date();
+        java.sql.Date sqlDate = new java.sql.Date(utilDate.getTime());
+        try {
+            int id = (Integer) j.get("id");
+            //System.out.println("id = " + id);
+            String volunteerIdStr = (String) j.get("volunteer_id");
+            int volunteerId = Integer.parseInt(volunteerIdStr);
+            String volunteerName = (String) j.get("volunteer_name");
+            //Date startTime= (Date)j.get("start_time");
+            //Date endTime= (Date)j.get("end_time");
+            String streetName = (String) j.get("street_name");
+            String houseNum = (String) j.get("tel_house_num");
+            String Address = houseNum + streetName;
+            String apt = (String) j.get("tel_apt");
+            String city = (String) j.get("txt_city");
+            String state = (String) j.get("txt_state");
+            String zip = (String) j.get("tel_zip");
+            String firstName = (String) j.get("txt_first_name");
+            String lastName = (String) j.get("txt_last_name");
+            String landlordName = (String) j.get("landlord_name");
+            String landlordPhone = (String) j.get("landlord_tel");
+            String dwellingType = (String) j.get("dwelling_type");
+            String insuranceInfo_f = (String) j.get("insurance_information_f");
+            String insuranceInfo_s = (String) j.get("insurance_information_s");
+            String insuranceInfo_c = (String) j.get("insurance_information_c");
+            String ownerInfo = (String) j.get("owner_information");
+            String floorNum = (String) j.get("number_floors");
+            int num_of_floor = -1;
+            int waterLivingInt = -1;
+            int waterBasementInt = -1;
+            if (floorNum != null && !floorNum.equals("")) {
+                num_of_floor = Integer.parseInt(floorNum);
+            }
+            String isBasement = (String) j.get("is_basement");
+            String isBasementOccupied = (String) j.get("is_basement_occupied");
+            String basementComment = (String) j.get("txtarea_basement_comment");
+            String waterLiving = (String) j.get("water_in_living_area");
+            if (waterLiving != null && !waterLiving.equals("")) {
+                waterLivingInt = Integer.parseInt(waterLiving);
+            }
+            String waterBasement = (String) j.get("water_in_basement");
+            if (waterBasement != null && !waterBasement.equals("")) {
+                waterBasementInt = Integer.parseInt(waterBasement);
+            }
+            String isElectricOn = (String) j.get("electric");
+            String isGasOn = (String) j.get("gas");
+            String Electrical_service_box = (String) j.get("d_electrical");
+            String Furnace = (String) j.get("d_furnace");
+            String Heat_Water_Heater = (String) j.get("d_water");
+            String Washer = (String) j.get("d_washer");
+            String Dryer = (String) j.get("d_dryer");
+            String Stove = (String) j.get("d_stove");
+            String Regfrigerator = (String) j.get("d_fridge");
+            String classification = (String) j.get("select_classification");
+            String reason = (String) j.get("txtArea_classification_reason");
+            String comments = (String) j.get("txtArea_comment");
 
+            Client client = new Client(Address, apt, city, state, zip, "", "", lastName, firstName);
+            client.Insert(con);
+            int clientId = client.getId(con);
+            System.out.println("clientId:" + clientId);
 
+            Building building = new Building(landlordName, landlordPhone, dwellingType, insuranceInfo_f, insuranceInfo_s, insuranceInfo_c, ownerInfo);
+            building.Insert(con);
 
+            int buildId = building.getId(con);
+            System.out.println("BuildingId:" + buildId);
+
+            DamageAssessment ds = new DamageAssessment(classification, "", "", num_of_floor, isBasement, waterLivingInt, waterBasementInt, isGasOn, isElectricOn, isBasementOccupied, basementComment, reason,
+            Electrical_service_box, Furnace, Heat_Water_Heater, Washer, Dryer, Stove, Regfrigerator, sqlDate, sqlDate);
+            ds.insert(con);
+            int damageAssessmentId = ds.getId(con);
+
+            Cases caseInstance = new Cases(comments, clientId, damageAssessmentId, buildId, volunteerId);
+            caseInstance.Insert(con);
+            return 1;
+        } catch (Exception e) {
+            return -1;
+        }
 
     }
 }
